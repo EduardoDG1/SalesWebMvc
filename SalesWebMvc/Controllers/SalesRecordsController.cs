5c2c897eb6f023cdffce81eda7458e95dd387e13
Eduardo Dalbem Gianechini <dudugianechini@gmail.com>
2024-03-05 11:00:51 -0300

    Sales view

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SimpleSearch()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
