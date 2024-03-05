a5d5e61efca995beda872db405c7a5f85803ee7c
Eduardo Dalbem Gianechini <dudugianechini@gmail.com>
2024-03-05 10:02:20 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -5,18 +5,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        [StringLength(60,MinimumLength = 3, ErrorMessage = "Name size should be between 3 and 60")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Email required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Birth Date required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "Base Salary required")]
+        [Range(100.0, 50000.0, ErrorMessage = "Base salary must be from {1} to {2}")]
         [Display(Name = "Base Salary")]
-
         [DataType(DataType.Currency)]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
